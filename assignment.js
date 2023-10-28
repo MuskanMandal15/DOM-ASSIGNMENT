8ecbc6bdb80ec2a90a05ecc43574f134a30d685a
muskanmandal <mandalmuskan12gmail.com>
2023-10-28 17:07:41 +0530

    added new files

---
+++ assignment.js
@@ -0,0 +1,11 @@
+const LI = document.querySelector('li:first-of-type');
+// const LI = document.querySelector("li");
+const TITLE = document.querySelector("title");
+const H1 = document.querySelector("h1");
+
+LI.style.backgroundColor = "black";
+LI.style.color = "white";
+
+TITLE.textContent = "Assignment-solved! "
+
+H1.textContent = "Assignment-solved!";
